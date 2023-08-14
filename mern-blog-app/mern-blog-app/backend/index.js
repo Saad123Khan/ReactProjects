b4f718c1afc38cfb651d5f9beaef235f968e100e
DESKTOP-PGA7OMU\Saad Khan <sk5908774@gmail.com>
2023-08-14 04:20:57 -0700

    first commit

---
+++ mern-blog-app/mern-blog-app/backend/index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
+
+dotenv.config();
+app.use(express.json());
+//app.use("/images", express.static(path.join(__dirname, "/images")));
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify:true
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+  var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "public/uploads");
+    },
+    filename: function (req, file, cb) {
+      cb(
+        null,
+        file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+      );
+    },
+  });
+  const maxSize = 2 * 1024 * 1024; // for 1MB
+   
+  var upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+      if (
+        file.mimetype == "image/png" ||
+        file.mimetype == "image/jpg" ||
+        file.mimetype == "image/jpeg"
+      ) {
+        cb(null, true);
+      } else {
+        cb(null, false);
+        return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
+      }
+    },
+    limits: { fileSize: maxSize },
+  }).single('file');
+   
+  app.post("/uploadfile",(req, res) => {
+    upload(req, res, function (err) {
+      if (err instanceof multer.MulterError) {
+        // A Multer error occurred when uploading.
+        res.send(err)
+      } else if (err) {
+        // An unknown error occurred when uploading.
+        res.send(err)
+      }
+    
+      console.log(req.file)
+      // Everything went fine.
+    })
+  });
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+app.listen("5000", () => {
+  console.log("Backend is running.");
+});
